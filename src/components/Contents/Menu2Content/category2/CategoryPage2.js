0f18dcf2ac80a19d1d3d22668fac3471b6335b99
pyunghun <564564q@naver.com>
2024-06-29 01:02:07 +0900

    categoryPage 1,2 추가

---
+++ src/components/Contents/Menu2Content/category2/CategoryPage2.js
@@ -0,0 +1,10 @@
+function CategoryPage2() {
+    console.log('categoryPage2')
+    return (
+        <div style={{width:'100%', height: '500px', backgroundColor:"pink"}}>
+            <p>여기에 카테고리 2의 내용이 표시됩니다.</p>
+        </div>
+    );
+}
+
+export default CategoryPage2;
