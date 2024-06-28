04d9fd435820967d92fcd771a16bd3aa75ca9bd3
pyunghun <564564q@naver.com>
2024-06-28 21:41:20 +0900

    menu1_layout

---
+++ src/components/Contents/Menu1Content/AiRecommend.js
@@ -0,0 +1,12 @@
+function AiRecommend(){
+
+    return(
+        <>
+            <div style={{width: '100%', height: '20vh', backgroundColor: 'gray'}}>
+
+            </div>
+        </>
+    );
+}
+
+export default AiRecommend;
--- src/components/Contents/Menu2.js
+++ src/components/Contents/Menu2.js
@@ -1,12 +1,27 @@
-function Menu2(){
+/*
+    두번째 메뉴 페이지 입니다.
+    작성일자 6월 28일
+*/
+import './Menu1.css';
+import AiRecommend from './Menu1Content/AiRecommend';
 
-    console.log('Menu2 영역입니다.')
+function Menu1(){
+
+    console.log('Mene2 영역입니다.')
 
     return(
         <>
-            <div style={{width: '100vw', height: '200px', backgroundColor: 'gray'}}></div>
+            <div className="menu2_layout">
+                <div className='ai_layout' style={{display: 'flex', justifyContent: 'center'}}>
+                    <div className='flex_wrap' style={{width: '90%'}}>
+                        <AiRecommend/>
+                        <button>1</button>
+                        <button>2</button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
 
-export default Menu2;
+export default Menu1;
