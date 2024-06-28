7c4c00a9bea06c920db51df066ba7a5d119cbb55
pyunghun <564564q@naver.com>
2024-06-28 19:36:20 +0900

    Header layout

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import Header from '../Header/Header';
-import Contents from '../Contents/Contents';
+import Menu1 from '../Contents/Menu1';
+import Menu2 from '../Contents/Menu2';
 import Footer from '../Footer/Footer';
 import './Main.css';
 
 function Main() {
+  console.log('메인페이지 입니다.');
+
+  //  content 이동 관련 state
+  const [menu, setMenu] = useState('menu1');  // 초기값은 content1
+
+  // Content 이동: Header Menu 에서 prop 로 받아온 값을 위한 함수
+  const changeMenu = (newMenu) => {
+    setMenu(newMenu);
+  };
+
+  // Header 컴포넌트를 useMemo를 사용하여 메모이제이션
+  const memoizedHeader = useMemo(() => <Header onChangeMenu={changeMenu} />, []);
+
+  // Footer 컴포넌트를 useMemo를 사용하여 메모이제이션
+  const memoizedFooter = useMemo(() => <Footer />, []);
+
   return (
     <div className="layout">
-      <Header />
-      <Contents />
-      <Footer />
+      {memoizedHeader}
+
+      {menu === 'menu1' && <Menu1 />}
+      {menu === 'menu2' && <Menu2 />}  
+
+      {memoizedFooter}
     </div>
   );
 }
